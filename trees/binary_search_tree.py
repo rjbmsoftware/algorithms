570b0caf09bebdc8c58455a16a5e17e6175ce221
richard.millett <richard.millett+github@googlemail.com>
2024-08-10 19:45:04 +0100

    Binary search tree insert, simplified test cases

--- trees/binary_search_tree.py
+++ trees/binary_search_tree.py
@@ -12,7 +12,7 @@ class BinarySearchTree:
     def __init__(self, root: Optional[TreeNode] = None):
         self._root = root
 
-    def insert(self, node: TreeNode, parent_node: Optional[TreeNode] = None) -> None:
+    def insert(self, node: TreeNode, subtree_root: Optional[TreeNode] = None) -> None:
         """
         If root node is None the new node will be root
         """
@@ -23,59 +23,70 @@ class BinarySearchTree:
             self._root = node
             return
 
-        if parent_node is None:
-            parent_node = self._root
+        if subtree_root is None:
+            subtree_root = self._root
 
-        if node.value <= parent_node.value:
-            if parent_node.left is None:
-                parent_node.left = node
+        if node.value <= subtree_root.value:
+            if subtree_root.left is None:
+                subtree_root.left = node
             else:
-                self.insert(node, parent_node.left)
+                self.insert(node, subtree_root.left)
         else:
-            self._root.right = node
+            if subtree_root.right is None:
+                subtree_root.right = node
+            else:
+                self.insert(node, subtree_root.right)
 
 
-class TestBinarySearchTree(unittest.TestCase):
+class TestBinarySearchTreeInsert(unittest.TestCase):
 
     def test_empty_tree_adding_a_node_is_new_root(self):
         bst = BinarySearchTree()
-        node = TreeNode()
-        bst.insert(node)
+        bst.insert(TreeNode(value=64))
 
-        self.assertEqual(bst._root, node)
+        self.assertEqual(bst._root.value, 64)
 
     def test_adding_nodes_with_values_less_than_root(self):
         root = TreeNode(value=64)
         bst = BinarySearchTree(root)
-        less_than_root_node = TreeNode(value=32)
-        bst.insert(less_than_root_node)
-        self.assertEqual(root.left, less_than_root_node)
+        bst.insert(TreeNode(value=32))
+        self.assertEqual(root.left.value, 32)
 
     def test_adding_nodes_with_values_equal_to_root(self):
         root = TreeNode(value=64)
         bst = BinarySearchTree(root)
-        equal_to_root_node = TreeNode(value=64)
-        bst.insert(equal_to_root_node)
-        self.assertEqual(root.left, equal_to_root_node)
+        bst.insert(TreeNode(value=64))
+        self.assertEqual(root.left.value, 64)
 
     def test_adding_nodes_with_values_more_than_root(self):
         root = TreeNode(value=64)
         bst = BinarySearchTree(root)
-        greater_than_root_node = TreeNode(value=96)
-        bst.insert(greater_than_root_node)
-        self.assertEqual(root.right, greater_than_root_node)
+        bst.insert(TreeNode(value=96))
+        self.assertEqual(root.right.value, 96)
 
     def test_minimum_value_is_left_most(self):
         root = TreeNode(value=64)
         bst = BinarySearchTree(root)
+        bst.insert(TreeNode(value=32))
+        bst.insert(TreeNode(value=16))
 
-        less_than_root = TreeNode(value=32)
-        bst.insert(less_than_root)
+        self.assertEqual(root.left.left.value, 16)
 
-        less_than_left_root = TreeNode(value=16)
-        bst.insert(less_than_left_root)
+    def test_maximum_value_is_right_most(self):
+        root = TreeNode(value=64)
+        bst = BinarySearchTree(root)
+        bst.insert(TreeNode(value=96))
+        bst.insert(TreeNode(value=128))
 
-        self.assertEqual(root.left.left, less_than_left_root)
+        self.assertEqual(root.right.right.value, 128)
+
+    def test_wiggle_values(self):
+        root = TreeNode(value=64)
+        bst = BinarySearchTree(root)
+        bst.insert(TreeNode(value=32))
+        bst.insert(TreeNode(value=48))
+
+        self.assertEqual(root.left.right.value, 48)
 
 
 if __name__ == '__main__':
