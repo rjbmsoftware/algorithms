dc6f0f424f42734b9bd22a1c51c965fc6de5f852
richard.millett <richard.millett+github@googlemail.com>
2024-09-01 11:25:17 +0100

    started AVL tree

---
+++ trees/avl_tree.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class AVLTree:
+    """
+    Implementation of the Adelson-Velsky and Landis self balancing binary search tree
+    """
+
+
+if __name__ == "__main__":
+    unittest.main()
