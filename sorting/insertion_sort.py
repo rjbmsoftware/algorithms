63db99d3d65b0294d6d3c0245d6ad4b17fc797e0
richard.millett <richard.millett+github@googlemail.com>
2024-10-05 10:06:51 +0100

    initial implementation of insertion sort

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -2,8 +2,19 @@ import unittest
 
 
 def insertion_sort(values: list) -> None:
-    # TODO move the is_sorted method out into a helper
-    pass
+    # TODO: split out is sorted method
+    # TODO: write up solution, comparison of shift vs swap
+    for working_index in range(1, len(values)):
+        current_value = values[working_index]
+        backwards_index = working_index
+        while backwards_index > 0 and values[backwards_index - 1] > current_value:
+            values[backwards_index] = values[backwards_index - 1]
+            backwards_index -= 1
+        values[backwards_index] = current_value
+
+
+def swap(values: list, first_index: int, second_index: int) -> None:
+    values[first_index], values[second_index] = values[second_index], values[first_index]
 
 
 class InsertionSortTest(unittest.TestCase):
@@ -25,6 +36,29 @@ class InsertionSortTest(unittest.TestCase):
 
         return True
 
+    def test_empty_list(self):
+        values = []
+        insertion_sort(values)
+        self.assertEqual(values, [])
+
+    def test_single_value_list(self):
+        values = [1]
+        insertion_sort(values)
+        self.assertEqual(len(values), 1)
+        self.assertTrue(values[0], 1)
+
+    def test_two_value_list(self):
+        values = [2, 1]
+        insertion_sort(values)
+        self.assertTrue(self.is_sorted(values),
+                        f'Expected [1, 2], actual {values}')
+
+    def test_three_value_list(self):
+        values = [3, 2, 1]
+        insertion_sort(values)
+        self.assertTrue(self.is_sorted(values),
+                        f'Expected [1, 2, 3], actual {values}')
+
     def test_max_to_min(self):
         input = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
         insertion_sort(input)
@@ -34,7 +68,7 @@ class InsertionSortTest(unittest.TestCase):
         input = []
         insertion_sort(input)
         self.assertTrue(self.is_sorted(input))
-        
+
     def test_already_sorted(self):
         input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         insertion_sort(input)
@@ -44,7 +78,6 @@ class InsertionSortTest(unittest.TestCase):
         input = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         insertion_sort(input)
         self.assertTrue(self.is_sorted(input))
-
 
 
 if __name__ == "__main__":
