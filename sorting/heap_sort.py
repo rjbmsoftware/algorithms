11c92f5c8fe77198ccc1a7a18a7a685c9a79ac8a
richard.millett <richard.millett+github@googlemail.com>
2024-10-10 17:31:23 +0100

    started heap sort

---
+++ sorting/heap_sort.py
@@ -0,0 +1,23 @@
+import unittest
+
+from sort_utils import is_sorted
+
+
+def heap_sort(values: list) -> None:
+    # TODO: implement
+    # sift up
+    # sift down
+    # extract min
+    # tree heap
+    # parent (i) is i / 2
+    # left child (i) 2*i
+    # right child is 2*i + 1
+    pass
+
+
+class HeapSortTest(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
