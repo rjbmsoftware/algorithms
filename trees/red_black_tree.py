98fabc57e7cb811119f22fdb07f5bb0df439981f
richard.millett <richard.millett+github@googlemail.com>
2024-11-11 17:16:15 +0000

    started insert using vim motions

--- trees/red_black_tree.py
+++ trees/red_black_tree.py
@@ -1,4 +1,43 @@
 import unittest
+from enum import StrEnum, auto
+
+
+class Colour(StrEnum):
+    RED = auto()
+    BLACK = auto()
+
+
+class ColourNode:
+
+    def __init__(self, value, colour, left_node, right_node):
+        self.value = value
+        self.colour: Colour = colour
+        self.left_node = left_node
+        self.right_node = right_node
+
+    @property
+    def colour(self):
+        return self.colour
+
+    @colour.setter
+    def colour(self, colour: Colour):
+        self.colour = colour
+
+    @property
+    def value(self) -> Colour:
+        return self.value
+
+    @value.setter
+    def value(self, value):
+        self.value = value
+
+    @property
+    def left(self):
+        return self.left
+
+    @left.setter
+    def left(self, new_left):
+        self.left = new_left
 
 
 class RedBlackTree:
@@ -15,19 +54,27 @@ class RedBlackTree:
         self._root = None
 
     @property
-    def root(self):
+    def root(self) -> ColourNode:
         return self._root
 
     def insert(self, value) -> None:
-        pass
+        """
+        Rules
+         1. if tree is empty create new node as root node with the colour black
+        """
+        if self.root is None:
+            self.root = ColourNode(value, Colour.BLACK, None, None)
 
     def delete(self, value) -> None:
         pass
 
 
 class TestRedBlackTree(unittest.TestCase):
-    pass
 
+    def test_insert_root_node_only(self):
+        tree = RedBlackTree()
+        tree.insert(10)
+        self.assertEqual(tree.root.colour, Colour.BLACK)
 
 if __name__ == "__main__":
     unittest.main()
