ab03386c3e922ebd9d885584949d3ca39fb93b9c
richard.millett <richard.millett+github@googlemail.com>
2024-12-03 20:59:48 +0000

    wip

--- dynamic_programming/knapsack.py
+++ dynamic_programming/knapsack.py
@@ -1,12 +1,26 @@
 import unittest
 
-def knapsack_optimiser(items: list[tuple[int, int]], capacity: int) -> list[tuple[int, int]]:
+def knapsack_optimiser(items: list[tuple[int, int]], total_capacity: int) -> list[tuple[int, int]]:
     """
     Work out which items for a given weight, value, and knapsack capacity have the highest
     overall value
+    
+    complexity:
+        time:
+
+        space:
+
+    Bottom up iterative approach
+
     """
-    capacities = [0] * capacity
+    capacities = [0] * total_capacity
     items_by_capacities = [capacities] * (len(items) + 1)
+    for capacity in range(1, len(items)):
+        for item in range(capacities[capacity]):
+            weight, value = items[capacity]
+            if weight < j:
+
+            items_by_capacities[capacity][j]
     return []
 
 class KnapsackTest(unittest.TestCase):
