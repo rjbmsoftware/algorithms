6548b67fc5dae8680274f0c2de6f28730386a7cd
richard.millett <richard.millett+github@googlemail.com>
2024-12-05 16:48:22 +0000

    knapsack part one done!

--- dynamic_programming/knapsack.py
+++ dynamic_programming/knapsack.py
@@ -1,22 +1,34 @@
 import unittest
 
 
-def knapsack_optimiser(items: list[tuple[int, int]], total_capacity: int) -> list[tuple[int, int]]:
+def knapsack_optimiser(items: list[tuple[int, int]], total_capacity: int) -> int:
     """
-    Work out which items for a given weight, value, and knapsack capacity have the highest
-    overall value
+    Calculate for a list of items with a given weight, value and capacity
+    what is largest value overall value we can have.
+
+    steps
+        create a 2D array of the capacity + 1 by the length of items + 1
+        filling the array with zeros, first row and column are used as
+        reference values.
+
+        for each item calculate if the item will fit plus the value of
+        the remaining capacity from the row above and that value is
+        greater than the value to the left of the current value.
 
     complexity:
         time:
-
+            O(nm) where n is the number of items and m is the total
+            capacity as the list of lists will be iterated over once
         space:
+            O(nm) where n is the number of items and m is the total
+            capacity
 
     Bottom up iterative approach
-
     """
-    items_by_capacities = []
-    for _ in range(len(items) + 1):
-        items_by_capacities.append([0] * (total_capacity + 1))
+
+    items_by_capacities = [[0] * (total_capacity + 1)
+                           for _ in range(len(items) + 1)]
+
     for i in range(1, len(items_by_capacities)):
         for j in range(1, total_capacity + 1):
             weight, value = items[i - 1]
@@ -25,22 +37,29 @@ def knapsack_optimiser(items: list[tuple[int, int]], total_capacity: int) -> lis
                     items_by_capacities[i - 1][j - weight]
             else:
                 items_by_capacities[i][j] = items_by_capacities[i - 1][j]
-    for items_by_capacity in items_by_capacities:
-        print(items_by_capacity)
-    return []
+
+    return items_by_capacities[-1][-1]
 
 
 class KnapsackTest(unittest.TestCase):
 
-    # def test_empty_items_empty_knapsack(self):
-    #     items = knapsack_optimiser([], 99)
-    #     self.assertFalse(items)
-
-    def test_example(self):
-        # Let, weight[] = {1, 2, 3}, profit[] = {10, 15, 40}, Capacity = 6
+    def test_all_items_fit(self):
         items = [(1, 10), (2, 15), (3, 40)]
         capacity = 6
-        output = knapsack_optimiser(items, capacity)
+        value = knapsack_optimiser(items, capacity)
+        self.assertEqual(value, 65)
+
+    def test_two_of_three_items_fit(self):
+        items = [(1, 10), (3, 15), (3, 40)]
+        capacity = 6
+        value = knapsack_optimiser(items, capacity)
+        self.assertEqual(value, 55)
+
+    def test_no_items_fit(self):
+        items = [(9, 15), (7, 40)]
+        capacity = 6
+        value = knapsack_optimiser(items, capacity)
+        self.assertEqual(value, 0)
 
 
 if __name__ == "__main__":
