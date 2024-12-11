336ba12d54b78a8426df1d745f816959af7cff71
richard.millett <richard.millett+github@googlemail.com>
2024-12-11 17:03:13 +0000

    started on unbound knapsack problem

---
+++ dynamic_programming/knapsack_unbound.py
@@ -0,0 +1,19 @@
+import unittest
+
+def knapsack_unbound(weights, values, capacity):
+    """
+    Unbound knapsack problem
+
+    steps
+
+    complexity:
+        time:
+        space:
+    """
+
+
+class KnapsackUnboundTest(unittest.main):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
