f384411a93a050daf78f450ad0231a6150a88856
richard.millett <richard.millett+github@googlemail.com>
2024-12-14 10:04:55 +0000

    wip

--- dynamic_programming/knapsack_recursive.py
+++ dynamic_programming/knapsack_recursive.py
@@ -1,10 +1,35 @@
 import unittest
 
-def knapsack(weights, values, capacity):
-    pass
+def knapsack(items, capacity):
+    if capacity or not items:
+        return 0
 
-class KnapsackTest(unittest.main):
-    pass
+    weight, value = items[0]
+
+    if weight <= capacity:
+        capacity -= weight
+        return value + knapsack(items[1:], capacity)
+    
+    
+
+class KnapsackTest(unittest.TestCase):
+    def test_all_items_fit(self):
+        items = [(1, 10), (2, 15), (3, 40)]
+        capacity = 6
+        value = knapsack(items, capacity)
+        self.assertEqual(value, 65)
+
+    def test_two_of_three_items_fit(self):
+        items = [(1, 10), (3, 15), (3, 40)]
+        capacity = 6
+        value = knapsack(items, capacity)
+        self.assertEqual(value, 55)
+
+    def test_no_items_fit(self):
+        items = [(9, 15), (7, 40)]
+        capacity = 6
+        value = knapsack(items, capacity)
+        self.assertEqual(value, 0)
 
 if __name__ == "__main__":
     unittest.main()
