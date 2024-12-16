1f803d363c080d40553f82406efac8bf26171228
richard.millett <richard.millett+github@googlemail.com>
2024-12-16 16:54:05 +0000

    wip

--- dynamic_programming/knapsack_recursive_memoisation.py
+++ dynamic_programming/knapsack_recursive_memoisation.py
@@ -1,48 +1,44 @@
 import unittest
 
-def knapsack(items, capacity, cache, index = 0):
 
-    if capacity <= 0 or index >= len(items):
-        return 0
+def knapsack(weights: list[int], values: list[int], capacity: int,
+             weight_value_cache: list[list[int]], index: int):
+    return 1
 
-    weight, value = items[index]
 
-    with_item_value = 0
-    if weight <= capacity:
-        with_item_value = value
-
-    next_index = index + 1
-
-    return max(
-        with_item_value + knapsack(items, capacity - weight, cache, next_index),
-        knapsack(items, capacity, cache, next_index)
-    )
-
-def knapsack_cache(items: list[tuple[int, int]], capacity: int) -> list[list[int]]:
-    return [[0] * capacity for _ in range(len(items))]
+def knapsack_cache(values: list[tuple[int, int]], capacity: int) -> list[list[int]]:
+    capacity += 1
+    return [[0] * capacity for _ in range(len(values))]
 
 
 class KnapsackTest(unittest.TestCase):
     def test_all_items_fit(self):
-        items = [(1, 10), (2, 15), (3, 40)]
+        weights = [1, 2, 3]
+        values = [10, 15, 40]
+        index = len(values) - 1
         capacity = 6
-        cache = knapsack_cache(items, capacity)
-        value = knapsack(items, capacity, cache)
+        cache = knapsack_cache(values, capacity)
+        value = knapsack(weights, values, capacity, cache, index)
         self.assertEqual(value, 65)
 
     def test_two_of_three_items_fit(self):
-        items = [(1, 10), (3, 15), (3, 40)]
+        weights = [1, 3, 3]
+        values = [10, 15, 40]
+        index = len(values) - 1
         capacity = 6
-        cache = knapsack_cache(items, capacity)
-        value = knapsack(items, capacity, cache)
+        cache = knapsack_cache(values, capacity)
+        value = knapsack(weights, values, capacity, cache, index)
         self.assertEqual(value, 55)
 
     def test_no_items_fit(self):
-        items = [(9, 15), (7, 40)]
+        weights = [9, 15]
+        values = [15, 40]
+        index = len(values) - 1
         capacity = 6
-        cache = knapsack_cache(items, capacity)
-        value = knapsack(items, capacity, cache)
+        cache = knapsack_cache(values, capacity)
+        value = knapsack(weights, values, capacity, cache, index)
         self.assertEqual(value, 0)
+
 
 if __name__ == "__main__":
     unittest.main()
