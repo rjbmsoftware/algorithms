e4eedf02372b3e868ca6bca1b889fdcf7d7b44a2
richard.millett <richard.millett+github@googlemail.com>
2024-12-19 17:18:53 +0000

    wip

--- dynamic_programming/knapsack_unbound.py
+++ dynamic_programming/knapsack_unbound.py
@@ -14,8 +14,30 @@ def knapsack_unbound(weights, values, capacity):
         pass
 
 
-class KnapsackUnboundTest(unittest.main):
-    pass
+class KnapsackUnboundTest(unittest.TestCase):
+    def test_all_items_fit(self):
+        weights = [1, 2, 3]
+        values = [10, 15, 40]
+        capacity = 6
+        value = knapsack_unbound(weights, values, capacity)
+        self.assertEqual(value, 65)
+
+    def test_two_of_three_items_fit(self):
+        weights = [1, 3, 3]
+        values = [10, 15, 40]
+        capacity = 6
+        value = knapsack_unbound(weights, values, capacity)
+        self.assertEqual(value, 55)
+
+    def test_no_items_fit(self):
+        weights = [9, 7]
+        values = [15, 40]
+        capacity = 6
+        value = knapsack_unbound(weights, values, capacity)
+        self.assertEqual(value, 0)
+
+if __name__ == "__main__":
+    unittest.main()
 
 if __name__ == '__main__':
     unittest.main()
