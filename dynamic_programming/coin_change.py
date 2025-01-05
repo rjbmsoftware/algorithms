06e1c69d3b097d7e32400b33c0a5b50d0f801326
richard.millett <richard.millett+github@googlemail.com>
2025-01-05 17:17:06 +0000

    trimmed down code and added complexity analysis

--- dynamic_programming/coin_change.py
+++ dynamic_programming/coin_change.py
@@ -1,10 +1,19 @@
 import unittest
 
+
 def coin_change(denominations: list[int], amount: int) -> int:
     """
-    Returns the lowest of coins for a given amount, amount
+    Returns the least of coins for a given amount, amount
     must be a multiple of the lowest denomination which must
     be one
+
+    complexity
+        time:
+            O(n * m), we build a cache of values and to fill the cache
+            each element is visited, n being the denominations for m 
+            amounts
+        space:
+            O(n * m), as above for storing the cache
     """
 
     matrix = [[0] * (amount + 1) for _ in range(len(denominations) + 1)]
@@ -13,25 +22,16 @@ def coin_change(denominations: list[int], amount: int) -> int:
             coin_value = denominations[i - 1]
             if coin_value <= j:
                 remainder = j % coin_value
-                if remainder == 0:
-                    if matrix[i - 1][j - 1] == 0:
-                        matrix[i][j] = j / coin_value
-                    else:
-                        coins_used = j // coin_value + matrix[i][remainder]
-                        if matrix[i - 1][j] > coins_used:
-                            matrix[i][j] = coins_used
-                        else:
-                            matrix[i][j] = matrix[i - 1][j]
+                if remainder == 0 and matrix[i - 1][j - 1] == 0:
+                    matrix[i][j] = j // coin_value
                 else:
                     coins_used = j // coin_value + matrix[i][remainder]
-                    if matrix[i - 1][j] > coins_used:
-                        matrix[i][j] = coins_used
-                    else:
-                        matrix[i][j] = matrix[i - 1][j]
+                    matrix[i][j] = min(coins_used, matrix[i - 1][j])
             else:
                 matrix[i][j] = matrix[i - 1][j]
 
     return matrix[-1][-1]
+
 
 class CoinChangeTest(unittest.TestCase):
 
@@ -49,6 +49,17 @@ class CoinChangeTest(unittest.TestCase):
         coins = [1, 2, 3]
         coins_used = coin_change(coins, 3)
         self.assertEqual(coins_used, 1)
+
+    def test_middle_denominations_uses_the_least_coins(self):
+        coins = [1, 2, 12, 20]
+        coins_used = coin_change(coins, 24)
+        self.assertEqual(coins_used, 2)
+
+    def test_smallest_denomination_uses_the_least_coins(self):
+        coins = [1, 10, 20]
+        coins_used = coin_change(coins, 3)
+        self.assertEqual(coins_used, 3)
+
 
 if __name__ == '__main__':
     unittest.main()
