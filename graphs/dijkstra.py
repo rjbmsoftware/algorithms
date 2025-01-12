22865d6e73de6c8d5311bb3a92f8f315ed5bfbc9
richard.millett <richard.millett+github@googlemail.com>
2025-01-12 13:12:48 +0000

    wip

--- graphs/dijkstra.py
+++ graphs/dijkstra.py
@@ -8,33 +8,58 @@ class Graph:
         """
         self.adjacency_matrix = [[0] * size for _ in range(size)]
         self.size = size
-        self.vertex_data = [''] * size
+        self.vertex_names = [''] * size
 
     def add_edge(self, u: int, v: int, weight: int) -> None:
         self.adjacency_matrix[u][v] = weight
         self.adjacency_matrix[v][u] = weight
 
-    def add_vertex_data(self, vertex: int, name: str) -> None:
+    def set_vertex_name(self, vertex: int, name: str) -> None:
         if 0 <= vertex < self.size:
-            self.vertex_data[vertex] = name
+            self.vertex_names[vertex] = name
 
-    def shortest_path(self, start_vertex):
+    def shortest_distance(self, start_vertex_name: str) -> list[float]:
         """
         Implementation of Dijkstra's shortest path algorithm
         """
         distances = [float('inf')] * self.size
-        start_vertex_index = self.vertex_data.index(start_vertex)
+        start_vertex_index = self.vertex_names.index(start_vertex_name)
         distances[start_vertex_index] = 0
         visited = [False] * self.size
+
+        for _ in range(self.size):
+            minimum_distance = float('inf')
+            u = None
+            for i in range(self.size):
+                if not visited[i] and distances[i] < minimum_distance:
+                    minimum_distance = distances[i]
+                    u = i
+
+            if u is None:
+                break
+
+            visited[u] = True
+
+            for v in range(self.size):
+                if self.adjacency_matrix[u][v] != 0 and not visited[v]:
+                    alt = distances[u] + self.adjacency_matrix[u][v]
+                    if alt < distances[v]:
+                        distances[v] = alt
+
+        return distances
 
 
 class ShortestPathTest(unittest.TestCase):
 
     def test_single_route(self):
-        graph = Graph(1)
-        graph.add_vertex_data(0, 'A')
-        graph.add_vertex_data(1, 'B')
+        graph = Graph(2)
+        graph.set_vertex_name(0, 'A')
+        graph.set_vertex_name(1, 'B')
         graph.add_edge(0, 1, 10)
+
+        distances = graph.shortest_distance('A')
+        self.assertEqual(distances[0], 0)
+        self.assertEqual(distances[1], 10)
 
 
 if __name__ == '__main__':
